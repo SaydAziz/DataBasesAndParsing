1d154647c4e332838ddb15f44105515d2c6f69bf
Aziz <aziz.saydaminov02@gmail.com>
2023-09-17 13:48:13 -0500

    Adds File IO Assignment.

---
+++ File IO/File IO/FileProperties.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_IO
+{
+    internal class FileProperties
+    {
+        public FileProperties(string path)
+        {
+            fileInfo = new FileInfo(path);
+            ExistingCharacters = new Dictionary<char, int>();
+            Name = fileInfo.Name;
+            FilePath = fileInfo.FullName;
+            CountCharacters();
+            
+        }
+
+        FileInfo fileInfo;
+        public string Name { get; set; }
+        public string FilePath { get; set; }
+        public Dictionary<char, int> ExistingCharacters { get; set; }
+
+        public void CountCharacters()
+        {
+            StreamReader sr = new StreamReader(FilePath);
+
+            do
+            {
+                char character = (char)sr.Read();
+                if (!Char.IsWhiteSpace(character))
+                {
+                    if (ExistingCharacters.ContainsKey(character))
+                    {
+                        ExistingCharacters[character]++;
+                    }
+                    else
+                    {
+                        ExistingCharacters.Add(character, 1);
+                    }
+                }
+                
+               
+            } while (!sr.EndOfStream);
+        }
+
+        public void ShowCharacters()
+        {
+            foreach (var character in ExistingCharacters) 
+            {
+                Console.WriteLine(character.Key + ": " + character.Value);
+
+            }
+        }
+
+    }
+}
---
+++ File IO/File IO/Program.cs
@@ -0,0 +1,34 @@
+﻿using File_IO;
+using System.IO;
+
+Console.WriteLine("Enter folder path: ");
+string path = Console.ReadLine();
+string[] files;
+Dictionary<string, FileProperties> fileData;
+
+if (!Directory.Exists(path))
+{
+    Console.WriteLine("Folder does not exist.");
+}
+else
+{
+    files = Directory.GetFiles(path);
+    FileInfo fileInfo;
+    fileData = files.ToDictionary(filePath => new FileInfo(filePath).Name, filePath => new FileProperties(filePath));
+
+    Console.WriteLine("\nFiles in folder: \n");
+    foreach (var file in fileData)
+    {
+        Console.WriteLine(file.Key);
+        file.Value.ShowCharacters();
+        Console.WriteLine(" ");
+
+
+    }
+
+
+}
+
+
+
+Console.ReadKey(true);
