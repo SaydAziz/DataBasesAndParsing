b35b82f27ff71bf4e6941047c41d7248819c4885
Aziz <aziz.saydaminov02@gmail.com>
2023-10-14 19:18:53 -0500

    Adds SQL assignment.

---
+++ ProduceDBUpdater/ProduceDBUpdater/Parser.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProduceDBUpdater
+{
+    public static class Parser
+    {
+        // ReadFile method reads the contents of a text file and splits it into lines based on the delimiter.
+        public static List<List<string>> ReadFile(string Path)
+        {
+            List<List<string>> Items = new List<List<string>>();
+            List<string> values;
+
+            // Use StreamReader to read the file line by line.
+            using (StreamReader sr = new StreamReader(Path))
+            {
+                string? currentLine = sr.ReadLine();
+                currentLine = sr.ReadLine();
+                while (currentLine != null)
+                {
+                    values = currentLine.Split('|').ToList();
+                    Items.Add(values);
+                    currentLine = sr.ReadLine();
+                }
+            }
+
+
+            return Items;
+
+        }
+
+        // WriteFile method writes the parsed data to a new text file.
+        public static void WriteFile(string path, List<string> items)
+        {
+            List<string> newItems = new List<string>();
+            // Create a new file name based on the original file name.
+            string newFileName = path.Substring(path.LastIndexOf('\\') + 1);
+            newFileName = newFileName.Insert(newFileName.LastIndexOf('.'), "_out");
+            newFileName = newFileName.Replace(newFileName.Substring(newFileName.LastIndexOf('.')), ".txt");
+
+            // Construct the new file path.
+            string newPath = path.Substring(0, path.LastIndexOf('\\') + 1) + "\\" + newFileName;
+
+            // Use FileStream and StreamWriter to create and write to the new file.
+            using (FileStream fs = File.Create(newPath))
+            {
+                using (StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine("Name,Location,Price,UoM,Sell_by_Date");
+                    foreach (string item in items)
+                    {
+                        //splitting string to reformat string values
+                        string[] newVals = item.Split('|');
+
+                        float price = float.Parse(newVals[2]);
+                        newVals[2] = price.ToString("0.00");
+
+                        newVals[4] = DateTime.Parse(newVals[4]).ToString("MM-dd-yyyy");
+                        //reconstructing string to print
+                        newItems.Add($"{newVals[0]}|{newVals[1]}|{newVals[2]}|{newVals[3]}|{newVals[4]}");
+                    }
+                    
+                    foreach(string item in newItems)
+                    {
+                        sw.WriteLine(item);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ProduceDBUpdater/ProduceDBUpdater/Program.cs
@@ -0,0 +1,82 @@
+﻿//Parser was taken from ParserEngine assignment
+//Replace query was inspired from Stack Overflow
+//GetDate was found on stack overflow
+//Rounding for float and Date parse was found on stack overflow
+
+using System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using ProduceDBUpdater;
+
+string path = Environment.CurrentDirectory + @"\TextFiles";
+string[] filePaths;
+List<List<string>> Produce;
+List<string> newProduce = new List<string>();
+
+// Get an array of file paths in the specified folder.
+filePaths = Directory.GetFiles(path);
+Produce = Parser.ReadFile(filePaths[0]);
+
+
+SqlConnectionStringBuilder sqlConBld = new SqlConnectionStringBuilder();
+sqlConBld["server"] = @"(localdb)\MSSQLLocalDB";
+sqlConBld["Trusted_Connection"] = true;
+sqlConBld["Integrated Security"] = "SSPI";
+sqlConBld["Initial Catalog"] = "PROG260FA23";
+string sqlConStr = sqlConBld.ToString();
+
+
+
+using (SqlConnection conn = new SqlConnection(sqlConStr))
+{
+    conn.Open();
+    foreach ( var item in Produce)
+    {
+        //Populating database with values read from file
+        string insertCommand = $@"INSERT [dbo].[Produce] ([Name], [Location], [Price], [UoM], [SellByDate]) VALUES ('{item[0]}', '{item[1]}', '{float.Parse(item[2])}', '{item[3]}', '{item[4]}')";
+        using (var command = new SqlCommand(insertCommand, conn))
+        {
+            var query = command.ExecuteNonQuery();
+        }
+    }
+    
+    string locSwitchCommand = $@"UPDATE Produce SET Location = REPLACE(Location, 'F', 'Z') WHERE Location LIKE '%f%';";
+    using (var command = new SqlCommand(locSwitchCommand, conn))
+    {
+        var query = command.ExecuteNonQuery();
+    }
+
+    string delExpiredCommand = $@"DELETE FROM Produce WHERE ([SellByDate] < GETDATE());";
+    using (var command = new SqlCommand(delExpiredCommand, conn))
+    {
+        var query = command.ExecuteNonQuery();
+    }
+
+    string addPriceCommand = $@"UPDATE Produce SET Price = Price + 1";
+    using (var command = new SqlCommand(addPriceCommand, conn))
+    {
+        var query = command.ExecuteNonQuery();
+    }
+    conn.Close();
+}
+
+//Reading database values into string list to print to file
+using (SqlConnection conn = new SqlConnection(sqlConStr))
+{
+    conn.Open();
+    string inLineSQL = @"Select * from Produce";
+    using (var command = new SqlCommand(inLineSQL, conn))
+    {
+        var reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            string item = $"{reader.GetValue(1)}|{reader.GetValue(2)}|{reader.GetValue(3)}|{reader.GetValue(4)}|{reader.GetValue(5)}";
+            newProduce.Add(item);
+        }
+        reader.Close();
+    }
+    conn.Close();
+}
+
+Parser.WriteFile(filePaths[0], newProduce);
